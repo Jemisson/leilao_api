85910fd0d8fd2887a7f5984151923c767366eb6b
Jemisson <jemison321@gmail.com>
2025-04-02 22:38:28 -0400

    ajusta cable_server.rb

--- cable_server.rb
+++ cable_server.rb
@@ -1,5 +1,6 @@
 # cable_server.rb
 require_relative 'config/environment'
 Rails.application.eager_load!
+Rails.application.config.force_ssl = false
 run ActionCable.server
 # This file is used to start the ActionCable server in a standalone mode.
